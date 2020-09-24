435cae72470b6881d615e9ef7a727c1966ac4ca5
Jawarny <jawarny@hotmail.fr>
2020-09-24 15:16:40 -0400

    Modification de la classe employeur à responsable admin

---
+++ GestionStageEquipe3/Areas/Stages/Models/ResponsableAdmin.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionStageEquipe3.Areas.Stages.Models
+{
+    public class ResponsableAdmin
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ResponsableAdminId { get; set; }
+
+        [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Prénom du responsable administratif")]
+        public string Prenom { get; set; }
+
+        [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Nom du responsable administratif")]
+        public string Nom { get; set; }
+
+        /**********************************************************************************/
+        [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Politesse du responsable administratif")]
+        public string PolitesseID { get; set; }
+        /**********************************************************************************/
+
+        /**********************************************************************************/
+        [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Titre du responsable administratif")]
+        public string TitreID { get; set; }
+        /**********************************************************************************/
+
+
+        [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Courriel du responsable administratif")]
+        public string Courriel { get; set; }
+
+        /* trouver comment mettre 10 min */
+        [StringLength(17, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Numéro de téléphone du responsable administratif")]
+        public string Telephone { get; set; }
+
+
+        [StringLength(4, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Numéro de Poste du responsable administratif")]
+        public int Poste { get; set; }
+
+        /* trouver comment mettre 10 min */
+        [StringLength(17, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Numéro cellulaire du responsable administratif")]
+        public string Cellulaire { get; set; }
+
+        /* trouver comment mettre 10 min */
+        [StringLength(17, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Numéro du télécopieur du responsable administratif")]
+        public string Telecopieur { get; set; }
+
+        [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "l'adresse du responsable administratif")]
+        public string Adresse { get; set; }
+
+        [StringLength(45, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "l'adresse du responsable administratif")]
+        public string Ville { get; set; }
+
+        /**********************************************************************************/
+        [StringLength(20, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Province du responsable administratif")]
+        public string Province { get; set; }
+        /**********************************************************************************/
+
+        [StringLength(7, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Code postal du responsable administratif")]
+        public string CodePostal { get; set; }
+
+        
+
+        
+
+        
+    }
+}
--- GestionStageEquipe3/Data/ApplicationDbContext.cs
+++ GestionStageEquipe3/Data/ApplicationDbContext.cs
@@ -19,7 +19,7 @@ namespace GestionStageEquipe3.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Employeur>().Property(e => e.EmployeurId).HasDefaultValueSql("newid()");
+            modelBuilder.Entity<ResponsableAdmin>().Property(e => e.ResponsableAdminId).HasDefaultValueSql("newid()");
         }
 
     }
