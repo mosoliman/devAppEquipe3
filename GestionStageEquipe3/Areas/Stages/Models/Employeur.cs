55819d10c4b5b9bb74b7e99deccf8f9380c5e2b5
Simon Nolet <rastanolet@gmail.com>
2020-09-24 11:41:46 -0400

    classe Politesse

--- GestionStageEquipe3/Areas/Stages/Models/Employeur.cs
+++ GestionStageEquipe3/Areas/Stages/Models/Employeur.cs
@@ -14,8 +14,19 @@ namespace GestionStageEquipe3.Areas.Stages.Models
         public Guid EmployeurId { get; set; }
 
         [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
-        [Display(Description = "Nom de l'entreprise")]
+        [Display(Description = "Prénom de l'employeur")]
+        public string PrénomEmployeur { get; set; }
+
+        [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Nom de l'employeur")]
         public string NomEmployeur { get; set; }
+
+        [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Courriel de l'employeur")]
+        public string CourrielEmployeur { get; set; }
+
+
+
 
         [Required(ErrorMessage = "Veuillez indiquer un statut")]
         [Display(Description = "Statut de l'employer")]
---
+++ GestionStageEquipe3/Areas/Stages/Models/Politesse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionStageEquipe3.Areas.Stages.Models
+{
+    public class Politesse
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid PolitesseId { get; set; }
+
+        [StringLength(200, ErrorMessage = "Vous devez entrer moins de caractères")]
+        [Display(Description = "Description de la Politesse")]
+        public string DescriptionPolitesse { get; set; }
+
+    }
+}
